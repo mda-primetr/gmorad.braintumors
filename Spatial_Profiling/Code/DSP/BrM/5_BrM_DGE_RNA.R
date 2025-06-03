1f7403a0910bc2d249c0dd6c4cc5ba347d8b9365
brendamelendez <146562086+brendamelendez@users.noreply.github.com>
2025-06-02 19:52:59 -0500

    Update 5_BrM_DGE_RNA.R

--- Spatial_Profiling/Code/DSP/BrM/5_BrM_DGE_RNA.R
+++ Spatial_Profiling/Code/DSP/BrM/5_BrM_DGE_RNA.R
@@ -91,7 +91,7 @@ ggplot(df_BIS_scores, aes(
     labs(x = "", y = "16S rRNA score")
 
 # Save pdf
-ggsave("Otput_files/DSp/Figures/FigureS4B2.pdf", width = 1.85, height = 2.2)
+ggsave("Otput_files/DSP/Figures/FigureS4B2.pdf", width = 1.85, height = 2.2)
 
 #----------------------------------------------------------------------------------------------------
 # Differential gene expression (Linear Mix Model)
